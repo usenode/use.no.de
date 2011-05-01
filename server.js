ee36678e84ce87acec2a50ad6cbfd25f53facb5f
Richard Hodgson <rightaboutnow@gmail.com>
2011-05-01 06:39:29 -0400

    Correctly lazy path argument to proton

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var proton = require('proton'),
     sys    = require('sys');
 
 proton.run(
-    require('./lib/webapp.js').WebApp,
+    require(__dirname + '/lib/webapp.js').WebApp,
     { port: 80 },
     function (e) {
         sys.error(e);
