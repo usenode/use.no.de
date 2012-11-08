2ea5841a74b9e9ca161c984a66846e2fb4e36715
Richard Hodgson <rightaboutnow@gmail.com>
2012-11-08 08:12:22 +0000

    Use jsgi directly until proton issue is resolved

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
+#!/bin/env node
+/*
+This should really just invoke Proton (use.no.de/proton) however it doesn't
+work with node v6.
+@see https://github.com/usenode/proton.js/issues/5
+*/
+var jsgi     = require('jsgi'),
+    http     = require('http'),
+    WebApp   = require('./lib/webapp').WebApp,
+    all      = require('promised-io/promise').all;
 
-var proton = require('proton'),
-    sys    = require('sys');
+var options = {
+    'bindTo': '0.0.0.0',
+    'port'  : 8080
+};
 
-proton.run(
-    require(__dirname + '/lib/webapp.js').WebApp,
-    { port: 80 },
-    function (e) {
-        sys.error(e);
-        process.exit(1);
-    }
-);
+var webapp = new WebApp();
 
+all(webapp._protonBeforeStart).then(function () {
+
+    var server = http.createServer(
+        new jsgi.Listener(function (request) {
+            return webapp.handle(request);
+        })
+    );
+
+    server.listen(options.port || 80, options.bindTo);
+
+    console.log(
+        'Webapp listening on ' +
+        (options.bindTo) +
+        ':' +
+        (options.port)
+    );
+});
